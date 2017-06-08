e707b5e32750a325b3791e216390431a855496d2
Prakash Shapkota <prakash.shapkota@marcelww.fr>
2017-06-08 16:35:19 +0200

    css test

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,11 @@
+require('../sass/main.scss');
+
 import React from "react";
 import { render } from "react-dom";
 import { browserHistory} from "react-router";
 import {BrowserRouter as Router, Route, Link, IndexRoute} from "react-router-dom";
+
+
 
 import {Root} from "./components/Root";
 import {Home} from "./components/Home";
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
+
 var path = require("path");
 
 var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
+
 
 var config = {
     entry: SRC_DIR + "/app/index.js",
     output: {
         path: DIST_DIR + "/app",
         filename: "bundle.js",
-        publicPath: "/app/"
+        publicPath: "/app/",
+        chunkFilename: "[id].js"
+
     },
     module: {
         loaders: [
@@ -19,9 +23,17 @@ var config = {
                 query: {
                     presets: ["react", "es2015", "stage-2"]
                 }
+            },
+            {
+                test: /\.scss$/,
+                loaders: [ 'style', 'css', 'sass' ]
             }
         ]
-    }
+    },
+    devtool: "source-map",
+    plugins: [
+        
+    ]
 };
 
 module.exports = config;
